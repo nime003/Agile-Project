c95d4d48a2fe4daaaf7e21c4d29f3f53c1ad752e
“Mebu98” <merijn.buitelaar98@gmail.com>
2023-06-05 21:38:38 +0200

    Basic navbar and hamburger menu

--- chew-app/client/src/App.js
+++ chew-app/client/src/App.js
@@ -4,67 +4,38 @@ import { TeacherPage } from "./pages/teacher/teacher";
 import { GamesPage } from "./pages/games/games";
 import DisplayRecipe from "./pages/games/displayRecipe";
 
-/*
-const testOppskrift = {
-    name: "Pannekaker",
-    scale: 1,
-    hvetemel: 0.75 * this.scale,
-    melk: 1.25 * this.scale,
-    salt: 0.13 * this.scale,
-    egg: 1 * this.scale,
-    smor: 0.25 * this.scale,
-    steps: [
-        `Bland ${(this.hvetemel)}dl mel og ${(this.salt)}ts salt. Tilsett ${this.melk/2}dl melk.`,
-        'Visp sammen til en tykk og klump-fri røre.',
-        `Tilsett resten av melken (${this.melk/2}dl). Visp inn ${(this.egg)} egg.`,
-        'La pannekakerøren svelle i ca. ½ time.',
-        'Smelt smør eller margarin i en god og varm stekepanne.',
-        'Hell i en øse med pannekakerøre og vend på pannen, slik at røren legger seg i et jevnt lag.',
-        'Snu pannekaken når den har stivnet på oversiden og blitt gyllenbrun på undersiden.',
-        'Når pannekaken er stekt på begge sider, brettes den sammen og legges i et ildfast fat med lokk. Pannekakene holder da varmen, slik at alle kan spise sammen.',
-        ],
-    quiz: {
-        topic: 'Pannekake oppskrift',
-        level: 'Enkel',
-        totalQuestions: 4,
-        questions: [
-            {
-                question: 'Hva er første steget i oppskriften?',
-                choices: ['Bland melk og salt., tilsett klump-fri røre.', 'Bland mel og salt, tilsett halvparten av melken.', 'Ingen av alternativene.'],
-                type: 'MCQs',
-                correctAnswer: 'Bland mel og salt, tilsett halvparten av melken.',
-            },
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import {Dropdown} from "react-bootstrap";
 
-            {
-                // Smelt smør eller margarin i en god og varm stekepanne.
-                // Hell i en øse med pannekakerøre og vend på pannen, slik at røren legger seg i et jevnt lag.
-                // Snu pannekaken når den har stivnet på oversiden og blitt gyllenbrun på undersiden.
-                question: 'Hvordan skal man helle røren i pannen?',
-                choices: ['Rett i og vente.', 'Helle i og vende på pannen slik at røren er gjevn.', 'Bruke vann i pannen og dyppe den i røren.', 'Ingen av alternativene.'],
-                type: 'MCQs',
-                correctAnswer: 'Helle i og vende på pannen slik at røren er gjevn.',
-            },
-            {
-                // Når pannekaken er stekt på begge sider, brettes den sammen og legges i et ildfast fat med lokk. Pannekakene holder da varmen, slik at alle kan spise sammen.
-                question:
-                    'Hvordan burde man lagre pannekaken når de er ferdigstekt?',
-                choices: ['I kjøleskapet.', 'På et varmt gulv.', 'Ute i sola.', 'Ingen av alternativene.'],
-                type: 'MCQs',
-                correctAnswer: 'Ingen av alternativene.',
-            },
-            {
-                // La pannekakerøren svelle i ca. ½ time.
-                question:
-                    'Hvor lenge skal pannekakerøren svelle?',
-                choices: ['1 time.', '1 minutt.', '30 minutter.', 'Ingen av alternativene.'],
-                type: 'MCQs',
-                correctAnswer: '30 minutter.',
-            }
-        ],
-    }
+function Navbar(){
+    return (
+        <nav className={"navbar"}
+        >
+            <Link to={"/"}>
+                <img src={process.env.PUBLIC_URL + "/images/Chew_Logo_Orange.png"} alt={"Chew Logo"}
+                     style={{width: "7vw", minWidth: "100px", height: "auto"}}
+                />
+            </Link>
+            <Dropdown
+                style={{ position:"absolute", right: "0px",marginRight: "2vw", width: "4vw", minWidth: "25px", height: "auto"}}
+            >
+                <Dropdown.Toggle variant="success" id="dropdown-basic"
+                                    style={{backgroundColor: "#ff5b2e", border: "none", }}
+                >
+                    <img src={process.env.PUBLIC_URL + "/images/burger-menu.svg"} alt={"Burger menu"}
+                         style={{width: "100%", minWidth: "30px", height: "auto"}}
+                    />
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                    <Dropdown.Item><Link to={"/"}>Home</Link></Dropdown.Item>
+                    <Dropdown.Item><Link to={"/teacher"}>Teacher</Link></Dropdown.Item>
+                    <Dropdown.Item><Link to={"/games"}>Games</Link></Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+        </nav>
+    );
 }
-*/
-
 
 function FrontPage() {
     return (
@@ -83,7 +54,7 @@ function FrontPage() {
 function App() {
   return (
       <BrowserRouter>
-          <Link to={"/"}>Home</Link>
+            <Navbar/>
           <Routes>
               <Route path={"/"} element={<FrontPage/>}></Route>
               <Route path={"/teacher/"} element={<TeacherPage/>}></Route>
