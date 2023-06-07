021819ab14a31088c7256edd04d75ba0be9ea46f
nime003 <nime003@student.kristiania.no>
2023-06-07 22:46:17 +0200

    Prøvd meg på å designe frontside

--- chew-app/client/src/App.js
+++ chew-app/client/src/App.js
@@ -1,17 +1,16 @@
-import React, {useState} from 'react';
-import './App.css';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import MenuIcon from '@mui/icons-material/Menu';
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route, Link, useNavigate } from 'react-router-dom';
+import { makeStyles } from '@mui/styles';
 import { styled } from '@mui/system';
 import { TeacherPage } from "./pages/teacher/teacher";
 import { RecipesPage } from "./pages/recipes/recipes";
 import DisplayRecipe from "./pages/recipes/displayRecipe";
 import LoginPage from "./pages/account/login";
 import ProfilePage from "./pages/account/profile";
-
+import MenuIcon from '@mui/icons-material/Menu';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import {Dropdown} from "react-bootstrap";
+import { Dropdown } from "react-bootstrap";
 import {
     AppBar,
     Box,
@@ -22,17 +21,96 @@ import {
     MenuItem,
     Switch,
     Toolbar,
-    Typography
+    Typography,
+    Button, Card, CardContent, Grid
 } from "@mui/material";
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import {AccountCircle} from "@mui/icons-material";
-import {grey} from "@mui/material/colors";
+import { AccountCircle } from "@mui/icons-material";
+import { grey } from "@mui/material/colors";
+import bgImage from "./background.JPG";
+import Logo from './ChewLogo.png';
 
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        position: 'relative',
+        height: '100vh',
+        overflow: 'hidden',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        padding: '0 30px',
+    },
+    background: {
+        backgroundImage: `url(${bgImage})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        position: 'absolute',
+        top: '-10%',
+        left: '-10%',
+        width: '120%',
+        height: '120%',
+        filter: 'blur(5px)',
+        zIndex: -1,
+    },
+    title: {
+        marginBottom: '30px',
+        fontWeight: 'bold',
+        color: '#fff',
+        textShadow: '2px 2px 4px #000',
+    },
+    cardContainer: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        gap: '20px',
+        marginBottom: '30px',
+    },
+    card: {
+        borderRadius: '10px',
+        boxShadow: '0px 0px 15px rgba(0, 0, 0, 0.1)',
+        cursor: 'pointer',
+        transition: 'transform 0.2s ease-in-out, box-shadow 0.2s ease-in-out',
+        '&:hover': {
+            transform: 'scale(1.03)',
+            boxShadow: '0px 0px 15px rgba(0, 0, 0, 0.3)',
+        },
+    },
+    cardButton: {
+        marginTop: '10px',
+    },
+}));
 
 function FrontPage() {
+    const classes = useStyles();
+    const navigate = useNavigate();
+
+    const boxes = [
+        { title: "Lag ferske og nydelige frokoster!", gradient: "linear-gradient(45deg, #f3e5f5 30%, #f6e68a 90%)" },
+        { title: "Lag enkle og avanserte middager for alle aldre!", gradient: "linear-gradient(45deg, #ffebee 30%, #ff8a80 90%)" },
+        { title: "Lag både søte og friske desserter", gradient: "linear-gradient(45deg, #e1bee7 30%, #ba68c8 90%)" },
+    ];
+
     return (
-        <div>
-            <h1>HELLO FRONT PAGE</h1>
+        <div className={classes.root}>
+            <div className={classes.background}></div>
+            <Typography variant="h3" className={classes.title}>Chew - Et digitalt matlagingskurs for barn!</Typography>
+            <Grid container justifyContent="center" spacing={3} className={classes.cardContainer}>
+                {boxes.map((box, index) => (
+                    <Grid item key={index} xs={12} sm={6} md={4} lg={3}>
+                        <Card className={classes.card} style={{ backgroundImage: box.gradient }}>
+                            <CardContent>
+                                <Typography variant="h5" component="h2">{box.title}</Typography>
+                                <Button variant="contained" color="primary" className={classes.button} onClick={() => navigate("/recipes")}>
+                                    Se mer
+                                </Button>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
         </div>
     );
 }
@@ -97,7 +175,17 @@ function NavBar(){
                     <Toolbar sx={{ display: 'flex', justifyContent: 'space-between'}}>
                         <Link to="/" style={{ color: 'inherit', textDecoration: 'none'}}>
                             <Typography variant="h6" noWrap component="div">
-                                Chew App
+                                <div style={{
+                                    borderRadius: '5px',
+                                    backgroundColor: 'white',
+                                    display: 'inline-block',
+                                    padding: '5px'
+                                }}>
+                                    <img src={Logo} alt="Logo" height="40px"
+                                         style={{
+                                             filter: 'drop-shadow(0px 0px 10px rgba(0, 0, 0, 0.15))'}}
+                                    />
+                                </div>
                             </Typography>
                         </Link>
                         <IconButton
@@ -125,18 +213,18 @@ function NavBar(){
 
 function App() {
 
-  return (
-      <BrowserRouter>
-        <NavBar/>
-          <Routes>
-              <Route path={"/"} element={<FrontPage/>} />
-              <Route path={"/teacher/"} element={<TeacherPage/>} />
-              <Route path={"/recipes/"} element={<RecipesPage/>} />
-              <Route path={"/login/"} element={<LoginPage/>} />
-              <Route path={"/profile"} element={<ProfilePage/>} />
-          </Routes>
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <NavBar/>
+            <Routes>
+                <Route path={"/"} element={<FrontPage/>} />
+                <Route path={"/teacher/"} element={<TeacherPage/>} />
+                <Route path={"/recipes/"} element={<RecipesPage/>} />
+                <Route path={"/login/"} element={<LoginPage/>} />
+                <Route path={"/profile"} element={<ProfilePage/>} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
