f55442265012cdcbb3c490161ea58eb164d45b89
“Mebu98” <merijn.buitelaar98@gmail.com>
2023-06-15 01:12:57 +0200

    changed images on profile page.

--- chew-app/client/src/pages/account/profile.js
+++ chew-app/client/src/pages/account/profile.js
@@ -1,27 +1,26 @@
 import React, { useState } from 'react';
-import { Container, Box, Typography, LinearProgress, List, ListItem, ListItemText, Button, AppBar, Toolbar, Avatar, Grid } from '@mui/material';
+import { Container, Box, Typography, LinearProgress, List, ListItem, ListItemText, Button, AppBar, Toolbar, Avatar } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import Dialog from '@mui/material/Dialog';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
-import profilePic1 from './images/1.jpg';
-import profilePic2 from './images/2.jpg';
-import profilePic3 from './images/3.jpg';
-import profilePic4 from './images/4.jpg';
-import profilePic5 from './images/5.jpg';
-import profilePic6 from './images/6.jpg';
-import profilePic7 from './images/7.jpg';
-import profilePic8 from './images/8.jpg';
-import profilePic9 from './images/9.jpg';
-import profilePic10 from './images/10.jpg';
-import profilePic11 from './images/11.jpg';
-import profilePic12 from './images/12.jpg';
-import profilePic13 from './images/13.jpg';
-import profilePic14 from './images/14.jpg';
 import {theme} from "../../App";
 import {ThemeProvider} from "@mui/material/styles";
 
-const profilePics = [profilePic1, profilePic2, profilePic3, profilePic4, profilePic5, profilePic6, profilePic7, profilePic8, profilePic9, profilePic10, profilePic11, profilePic12, profilePic13, profilePic14];
+
+const profilePics =[
+    // Pictures from https://www.flaticon.com/packs/animals-123
+    "images/avatars/bear.png",
+    "images/avatars/chicken.png",
+    "images/avatars/hen.png",
+    "images/avatars/koala.png",
+    "images/avatars/owl.png",
+    "images/avatars/panda.png",
+    "images/avatars/penguin.png",
+    "images/avatars/puffer-fish.png",
+    "images/avatars/rabbit.png",
+    "images/avatars/sloth.png",
+];
 
 const ProfilePage = () => {
     const navigate = useNavigate();
@@ -57,7 +56,7 @@ const ProfilePage = () => {
                 <Container maxWidth="md">
                     <Box sx={{ my: 4 }}>
                         <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                            <Avatar alt="Profile Picture" src={profilePic} sx={{ width: 56, height: 56, cursor: 'pointer' }} onClick={handleClickOpen} />
+                            <Avatar alt="Profile Picture" src={process.env.PUBLIC_URL + profilePic} sx={{ width: 56, height: 56, cursor: 'pointer' }} onClick={handleClickOpen} />
                             <Typography variant="h4" component="h1" gutterBottom sx={{ ml: 2 }}>
                                 Admin
                             </Typography>
